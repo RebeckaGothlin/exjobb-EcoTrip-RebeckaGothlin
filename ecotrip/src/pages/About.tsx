12df7b3cca1d7a1d785b89b43e99a733cd8113f2
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-10-25 14:11:43 +0200

    :sparkles: add content to about page

--- ecotrip/src/pages/About.tsx
+++ ecotrip/src/pages/About.tsx
@@ -1,8 +1,31 @@
 import { Header } from "../components/Header";
+import {
+  ParagraphText,
+  TextContainer,
+  TextTitle,
+} from "../components/styled/StyledContent";
 
 export const About = () => {
-  return <>
-  <Header></Header>
-  About
-  </>;
+  return (
+    <>
+      <Header></Header>
+      <TextContainer>
+        <TextTitle>About</TextTitle>
+        <ParagraphText>
+          This website was developed as part of my final thesis project during
+          my studies in Frontend Development at Medieinstitutet. I am passionate
+          about environmental sustainability and recognize the importance of
+          reducing our carbon footprint. This platform aims to provide users
+          with the ability to calculate carbon emissions for various modes of
+          transportation between different cities. By offering this tool, I hope
+          to raise awareness about the impact of our travel choices and
+          encourage individuals to make more eco-friendly decisions. My vision
+          is to create a user-friendly experience that empowers everyone to
+          understand their carbon emissions and take actionable steps towards a
+          more sustainable future. Thank you for visiting, and I hope you find
+          this tool valuable in your journey toward sustainability.
+        </ParagraphText>
+      </TextContainer>
+    </>
+  );
 };
--- ecotrip/src/pages/Home.tsx
+++ ecotrip/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import { Header } from "../components/Header";
-import { TextContainer, TextTitle } from "../components/styled/StyledContent";
+import {
+  ParagraphText,
+  TextContainer,
+  TextTitle,
+} from "../components/styled/StyledContent";
 import { StyledLink } from "../components/styled/StyledLinks";
 
 export const Home = () => {
@@ -8,15 +12,17 @@ export const Home = () => {
       <Header></Header>
       <TextContainer>
         <TextTitle>Introduction</TextTitle>
-        Traveling is a hobby for many, but it comes at a cost to the
-        environment. Transport options like flights and cars release significant
-        amounts of carbon dioxide, contributing to climate change, which affects
-        our health and the planet. On EcoTrip, you can compare emissions from
-        different transport modes like flights, cars, and trains, helping you
-        make more informed choices to reduce your environmental impact. Click on
-        “Calculate” in the top navigation bar to compare emissions for different
-        transport options, or click{" "}
-        <StyledLink href="/calculate">here</StyledLink> to go there directly.
+        <ParagraphText>
+          Traveling is a hobby for many, but it comes at a cost to the
+          environment. Transport options like flights and cars release
+          significant amounts of carbon dioxide, contributing to climate change,
+          which affects our health and the planet. On EcoTrip, you can compare
+          emissions from different transport modes like flights, cars, and
+          trains, helping you make more informed choices to reduce your
+          environmental impact. Click on “Calculate” in the top navigation bar
+          to compare emissions for different transport options, or click{" "}
+          <StyledLink href="/calculate">here</StyledLink> to go there directly.
+        </ParagraphText>
       </TextContainer>
     </>
   );
