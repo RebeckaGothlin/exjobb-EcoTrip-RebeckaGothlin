86f69a2564ba8929e0ec8e8303bb7c8fc0082139
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-10-30 17:06:20 +0100

    add nothing important - just for fun

--- ecotrip/src/pages/Calculate.tsx
+++ ecotrip/src/pages/Calculate.tsx
@@ -6,8 +6,24 @@ import {
 } from "../components/styled/StyledContent";
 import { Form, Input, Para } from "../components/styled/StyledForm";
 import { Button } from "../components/styled/StyledButtons";
+import { FormEvent, useState } from "react";
 
 export const Calculate = () => {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = () => {
+    setResult(from + to);
+  }
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    handleClick();
+    setFrom("");
+    setTo("");
+  }
+
   return (
     <>
       <Header></Header>
@@ -19,13 +35,16 @@ export const Calculate = () => {
           from and the city you want to travel to, then press the "Calculate"
           button to estimate emissions for different travel methods – plane,
           car, bus, and train.
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Para>I want to travel from:</Para>
-            <Input type="text" placeholder="From.." />
+            <Input type="text" value={from} placeholder="From.." onChange={(e) => setFrom(e.target.value)}/>
             <Para>I want to travel to:</Para>
-            <Input type="text" placeholder="To.." />
-            <Button>Calculate</Button>
+            <Input type="text" value={to} placeholder="To.." onChange={(e) => setTo(e.target.value)}/>
+            <Button onClick={handleClick}>Calculate</Button>
           </Form>
+          {result && (
+            <ParagraphText>{result}</ParagraphText>
+          )}
         </ParagraphText>
       </TextContainer>
     </>
