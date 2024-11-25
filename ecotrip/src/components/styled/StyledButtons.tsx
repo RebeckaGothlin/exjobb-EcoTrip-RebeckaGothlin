ca731da88559c8213135de2563931042610496aa
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-11-25 08:15:59 +0100

    :bug: fix nav button

--- ecotrip/src/components/styled/StyledButtons.tsx
+++ ecotrip/src/components/styled/StyledButtons.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const ContentButton = styled.button`
-  background: ${({ theme }) => theme.name === "Dark" 
-    ? theme.buttonBackgroundColor 
-    : theme.buttonBackgroundColor};
-  color: ${({ theme }) => theme.buttonTextColor}; 
-  border: ${({ theme }) => theme.name === "Light" ? theme.buttonBorderColor : "none"};
+  background: ${({ theme }) =>
+    theme.name === "Dark"
+      ? theme.buttonBackgroundColor
+      : theme.buttonBackgroundColor};
+  color: ${({ theme }) => theme.buttonTextColor};
+  border: ${({ theme }) =>
+    theme.name === "Light" ? theme.buttonBorderColor : "none"};
   display: block;
   margin: 0 auto;
   margin-top: 20px;
@@ -20,20 +22,21 @@ export const ContentButton = styled.button`
   touch-action: manipulation;
 
   &:hover {
-    /* box-shadow: ${({ theme }) => theme.name === "Dark" 
-      ? "rgba(255, 148, 7, 0.35) 0 -25px 18px -14px inset, rgba(44, 187, 99, .25) 0 1px 2px, rgba(44, 187, 99, .25) 0 2px 4px, rgba(44, 187, 99, .25) 0 4px 8px, rgba(44, 187, 99, .25) 0 8px 16px, rgba(44, 187, 99, .25) 0 16px 32px"
-      : "0 0 10px rgba(0, 0, 0, 0.1)"}; */
+    /* box-shadow: ${({ theme }) =>
+      theme.name === "Dark"
+        ? "rgba(255, 148, 7, 0.35) 0 -25px 18px -14px inset, rgba(44, 187, 99, .25) 0 1px 2px, rgba(44, 187, 99, .25) 0 2px 4px, rgba(44, 187, 99, .25) 0 4px 8px, rgba(44, 187, 99, .25) 0 8px 16px, rgba(44, 187, 99, .25) 0 16px 32px"
+        : "0 0 10px rgba(0, 0, 0, 0.1)"}; */
     transform: scale(1.05) rotate(-1deg);
   }
 
   &:focus {
     outline: none;
-  }  
+  }
 `;
 export const LinkButton = styled.button`
   background-color: white;
   font-weight: 400;
-  font-family: 'Poppins', serif;
+  font-family: "Poppins", serif;
   padding: 0 15px;
   height: 40px;
   font-size: 0.9rem;
@@ -51,3 +54,34 @@ export const LinkButton = styled.button`
   }
 `;
 
+export const NavButton = styled.button`
+  background-color: white;
+  font-weight: 400;
+  font-family: "Poppins", serif;
+  padding: 0 25px;
+  height: 35px;
+  border: 1.5px solid
+    ${({ theme }) => (theme.name === "Light" ? "black" : "white")};
+  color: ${({ theme }) => (theme.name === "Light" ? "black" : "white")};
+  margin-left: auto;
+  padding: 0 3px;
+  height: 30px;
+  font-size: 0.9rem;
+  margin-top: 5px;
+  color: black;
+  /* border: 1.5px solid ${({ theme }) =>
+    theme.name === "Light" ? "black" : "white"};
+  color: ${({ theme }) => (theme.name === "Light" ? "black" : "white")}; */
+  cursor: pointer;
+  &:hover {
+    border: 2px solid
+      ${({ theme }) => (theme.name === "Light" ? "black" : "white")};
+    @media (min-width: 768px) {
+      padding: 0 10px;
+      height: 40px;
+      font-size: 1.2rem;
+      margin-top: 10px;
+      font-weight: 500;
+    }
+  }
+`;
