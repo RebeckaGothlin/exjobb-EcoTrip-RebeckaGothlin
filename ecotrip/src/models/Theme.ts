ba25a274b8bcf4a8c02d1f984d56dd09f0f6b3e1
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-11-25 16:43:48 +0100

    :lipstick: change color on buttons

--- ecotrip/src/models/Theme.ts
+++ ecotrip/src/models/Theme.ts
@@ -27,28 +27,35 @@ export const themes: IThemes = {
         navBackgroundColor: "rgba(0, 0, 0, 0.8)",
         navTextColor: "white",
         activeNavTextColor: "white",
-        // textContainerBackgroundColor: `
-        // radial-gradient(circle at 30% 30%, rgba(128, 0, 128, 0.4), transparent 50%),
-        // radial-gradient(circle at 70% 70%, rgba(255, 105, 180, 0.3), transparent 60%),
-        // radial-gradient(circle at 50% 90%, rgba(255, 165, 0, 0.2), transparent 70%),
-        // #000000`,
-
         textContainerBackgroundColor: `
-        radial-gradient(circle at 30% 30%, rgba(0, 0, 139, 0.2), transparent 50%),   /* Mörkblå */
-        radial-gradient(circle at 70% 70%, rgba(0, 191, 255, 0.15), transparent 60%), /* Vattenturkos */
-        radial-gradient(circle at 50% 90%, rgba(0, 0, 255, 0.1), transparent 70%),    /* Blå */
-        #000000; /* Svart */
-        background-size: 200% 200%;
-        animation: moveBackground 20s infinite linear;
-      `,
+            radial-gradient(circle at 30% 30%, rgba(75, 0, 75, 0.4), transparent 50%),
+            radial-gradient(circle at 70% 70%, rgba(178, 41, 98, 0.3), transparent 60%),
+            radial-gradient(circle at 50% 90%, rgba(139, 0, 66, 0.2), transparent 10%),
+            #000000`,
+
+
+
+        //     textContainerBackgroundColor: `
+        //     radial-gradient(circle at 30% 30%, rgba(0, 0, 139, 0.2), transparent 50%),  
+        //     radial-gradient(circle at 70% 70%, rgba(0, 191, 255, 0.15), transparent 60%),
+        //     radial-gradient(circle at 50% 90%, rgba(0, 0, 255, 0.1), transparent 70%),   
+        //     #000000; /* Svart */
+        //     background-size: 200% 200%;
+        //     animation: moveBackground 20s infinite linear;
+        //   `,
+
 
 
         textContainerTextColor: "white",
-        buttonBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(173, 216, 230, 0.8))", /* Mörkblå till ljusblå */
+        buttonBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(255, 105, 180, 0.8))",
+
         buttonTextColor: "white",
         buttonBorderColor: "rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px",
         // sliderBackgroundColor: "linear-gradient(#004E41, #51b8a7)",
-        sliderBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(173, 216, 230, 0.8))",  /* Mörkblå (rgba(0, 0, 139)) till ljusblå (rgba(173, 216, 230)) */
+        // sliderBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(173, 216, 230, 0.8))", 
+        sliderBackgroundColor: "linear-gradient(225deg, rgba(128, 0, 168, 0.8), rgba(255, 105, 180, 0.8), rgba(255, 165, 0, 0.8))",
+
+
         sliderBorderColor: "none",
     },
     light: {
