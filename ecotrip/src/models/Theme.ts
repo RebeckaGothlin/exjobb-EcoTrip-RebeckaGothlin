f7246eab7a66782b62939cc2ac04eff5b8fcc456
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-11-27 10:16:26 +0100

    :lipstick: change colors on dark theme

--- ecotrip/src/models/Theme.ts
+++ ecotrip/src/models/Theme.ts
@@ -20,44 +20,23 @@ interface IThemes {
 }
 
 export const themes: IThemes = {
-    dark: {
-        name: "Dark",
-        linkButtonBackgroundColor: "white",
-        linkButtonTextColor: "black",
-        navBackgroundColor: "rgba(0, 0, 0, 0.8)",
-        navTextColor: "white",
-        activeNavTextColor: "white",
-        textContainerBackgroundColor: `
-            radial-gradient(circle at 30% 30%, rgba(75, 0, 75, 0.4), transparent 50%),
-            radial-gradient(circle at 70% 70%, rgba(178, 41, 98, 0.3), transparent 60%),
-            radial-gradient(circle at 50% 90%, rgba(139, 0, 66, 0.2), transparent 10%),
-            #000000`,
+dark: {
+    name: "Dark",
+    linkButtonBackgroundColor: "transparent", 
+    linkButtonTextColor: "white", 
+    navBackgroundColor: "rgba(20, 20, 20, 0.95)", 
+    navTextColor: "white", 
+    activeNavTextColor: "white",
+    textContainerBackgroundColor: "#1a1a1a",
+    textContainerTextColor: "white", 
+    buttonBackgroundColor: "transparent", 
+    buttonTextColor: "white", 
+    buttonBorderColor: "white",
+    sliderBackgroundColor: "linear-gradient(45deg, #ED3975, #ff9f71)",
+    sliderBorderColor: "none", 
+},
 
-
-
-        //     textContainerBackgroundColor: `
-        //     radial-gradient(circle at 30% 30%, rgba(0, 0, 139, 0.2), transparent 50%),  
-        //     radial-gradient(circle at 70% 70%, rgba(0, 191, 255, 0.15), transparent 60%),
-        //     radial-gradient(circle at 50% 90%, rgba(0, 0, 255, 0.1), transparent 70%),   
-        //     #000000; /* Svart */
-        //     background-size: 200% 200%;
-        //     animation: moveBackground 20s infinite linear;
-        //   `,
-
-
-
-        textContainerTextColor: "white",
-        buttonBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(255, 105, 180, 0.8))",
-
-        buttonTextColor: "white",
-        buttonBorderColor: "rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px",
-        // sliderBackgroundColor: "linear-gradient(#004E41, #51b8a7)",
-        // sliderBackgroundColor: "linear-gradient(45deg, rgba(0, 0, 139, 0.8), rgba(173, 216, 230, 0.8))", 
-        sliderBackgroundColor: "linear-gradient(225deg, rgba(128, 0, 168, 0.8), rgba(255, 105, 180, 0.8), rgba(255, 165, 0, 0.8))",
-
-
-        sliderBorderColor: "none",
-    },
+    
     light: {
         name: "Light",
         linkButtonBackgroundColor: "rgba(255, 245, 250, 1)",
