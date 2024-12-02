bc382c4802dd8b3ab81bde2fcd84297eb2c66eba
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-12-02 08:32:13 +0100

    :lipstick: style buttons + leaflet map

--- ecotrip/src/components/styled/StyledButtons.tsx
+++ ecotrip/src/components/styled/StyledButtons.tsx
@@ -25,7 +25,7 @@ export const ContentButton = styled.button`
       theme.name === "Dark"
         ? "rgba(255, 148, 7, 0.35) 0 -25px 18px -14px inset, rgba(44, 187, 99, .25) 0 1px 2px, rgba(44, 187, 99, .25) 0 2px 4px, rgba(44, 187, 99, .25) 0 4px 8px, rgba(44, 187, 99, .25) 0 8px 16px, rgba(44, 187, 99, .25) 0 16px 32px"
         : "0 0 10px rgba(0, 0, 0, 0.1)"}; */
-    transform: scale(1.05) rotate(-1deg);
+    transform: scale(1.05);
   }
 
   &:focus {
@@ -45,6 +45,10 @@ export const LinkButton = styled.button`
   color: ${({ theme }) => theme.linkButtonTextColor};
   background: ${({ theme }) => theme.navBackgroundColor};
   cursor: pointer;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 
   @media (min-width: 768px) {
     padding: 0 20px;
@@ -107,13 +111,14 @@ export const HistorySaveButton = styled.button`
   -webkit-user-select: none;
   touch-action: manipulation;
   border: 1.5px solid ${({ theme }) => theme.buttonBorderColor};
+  margin-top: 100px;
 
   &:hover {
     /* box-shadow: ${({ theme }) =>
       theme.name === "Dark"
         ? "rgba(255, 148, 7, 0.35) 0 -25px 18px -14px inset, rgba(44, 187, 99, .25) 0 1px 2px, rgba(44, 187, 99, .25) 0 2px 4px, rgba(44, 187, 99, .25) 0 4px 8px, rgba(44, 187, 99, .25) 0 8px 16px, rgba(44, 187, 99, .25) 0 16px 32px"
         : "0 0 10px rgba(0, 0, 0, 0.1)"}; */
-    transform: scale(1.05) rotate(-1deg);
+    transform: scale(1.05);
   }
 
   &:focus {
