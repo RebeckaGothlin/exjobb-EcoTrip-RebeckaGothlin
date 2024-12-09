751040c6b34db26c8a0b69f7062a56a5268dc143
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-12-09 08:04:59 +0100

    🔥 remove commented out code in home.tsx

--- ecotrip/src/pages/Home.tsx
+++ ecotrip/src/pages/Home.tsx
@@ -1,9 +1,6 @@
 import { Header } from "../components/Header";
 import {
   GlobalWrapper,
-  // ParagraphText,
-  // TextContainer,
-  // TextTitle,
 } from "../components/styled/StyledContent";
 
 export const Home = () => {
@@ -12,19 +9,6 @@ export const Home = () => {
       <GlobalWrapper>
         <Header></Header>
       </GlobalWrapper>
-      {/* <TextContainer>
-        <TextTitle>Introduction</TextTitle>
-        <ParagraphText>
-          Traveling is a hobby for many, but it comes at a cost to the
-          environment. Transport options like flights and cars release
-          significant amounts of carbon dioxide, contributing to climate change,
-          which affects our health and the planet. On EcoTrip, you can compare
-          emissions from different transport modes like flights, cars, and
-          trains, helping you make more informed choices to reduce your
-          environmental impact. Click on “Calculate” in the top navigation bar
-          to compare emissions for different transport options.
-        </ParagraphText>
-      </TextContainer> */}
     </>
   );
 };
