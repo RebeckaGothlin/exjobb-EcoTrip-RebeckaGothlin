181d5ff75c7106fc8b333041832cc0264f8120c6
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2025-01-14 15:48:24 +0100

    :sparkles: add title to navlink

--- ecotrip/src/components/Navbar.tsx
+++ ecotrip/src/components/Navbar.tsx
@@ -19,47 +19,74 @@ export const Navbar = () => {
     if (event.key === "Enter" || event.key === " ") {
       toggleMenu();
     }
-  }
+  };
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-
   return (
     <>
-        <Nav>
-          <NavMenu>
-            {/* <NavLinkStyledTitle to="/" onClick={toggleMenu}>
+      <Nav>
+        <NavMenu>
+          <NavItem>
+            <NavLinkStyledTitleDesktop
+              to="/"
+              aria-label="Go to EcoTrip homepage"
+              title="Go to Home page"
+            >
               EcoTrip
-            </NavLinkStyledTitle> */}
-            <NavItem>
-              <NavLinkStyledTitleDesktop to="/" aria-label="Go to EcoTrip homepage">EcoTrip</NavLinkStyledTitleDesktop>
-            </NavItem>
-            <NavItem>
-              <NavLinkStyled to="/calculate" aria-label="Go to the Calculate page">Calculate</NavLinkStyled>
-            </NavItem>
-            <NavItem>
-              <NavLinkStyled to="/info" aria-label="Learn more about eco-friendly travel">Information</NavLinkStyled>
-            </NavItem>
-            <NavItem>
-              <NavLinkStyled to="/faq" aria-label="About EcoTrip">FAQ</NavLinkStyled>
-            </NavItem>
-            {/* <NavItem>
-              <NavLinkStyled to="/about" aria-label="About EcoTrip">About</NavLinkStyled>
-            </NavItem> */}
-            <NavItem className="last-item">{/* <ThemeToggle /> */}</NavItem>
-          </NavMenu>
-          <HamburgerMenuButton             role="button"
-            onClick={toggleMenu}
-            onKeyDown={handleKeyDown}
-            aria-label={menuOpen ? "Close menu" : "Open menu"}
-            tabIndex={0}>
+            </NavLinkStyledTitleDesktop>
+          </NavItem>
+          <NavItem>
+            <NavLinkStyled
+              to="/calculate"
+              aria-label="Go to the Calculate page"
+            >
+              Calculate
+            </NavLinkStyled>
+          </NavItem>
+          <NavItem>
+            <NavLinkStyled
+              to="/info"
+              aria-label="Learn more about eco-friendly travel"
+            >
+              Information
+            </NavLinkStyled>
+          </NavItem>
+          <NavItem>
+            <NavLinkStyled to="/faq" aria-label="About EcoTrip">
+              FAQ
+            </NavLinkStyled>
+          </NavItem>
+          <NavItem className="last-item"></NavItem>
+        </NavMenu>
+        <HamburgerMenuButton
+          role="button"
+          onClick={toggleMenu}
+          onKeyDown={handleKeyDown}
+          aria-label={menuOpen ? "Close menu" : "Open menu"}
+          tabIndex={0}
+        >
           {menuOpen ? (
             <>
-              <span className="line" style={{ transform: "rotate(45deg)", position: "relative", top: "8.5px" }} />
+              <span
+                className="line"
+                style={{
+                  transform: "rotate(45deg)",
+                  position: "relative",
+                  top: "8.5px",
+                }}
+              />
               <span className="line" style={{ opacity: 0 }} />
-              <span className="line" style={{ transform: "rotate(-45deg)", position: "relative", top: "-8px" }} />
+              <span
+                className="line"
+                style={{
+                  transform: "rotate(-45deg)",
+                  position: "relative",
+                  top: "-8px",
+                }}
+              />
             </>
           ) : (
             <>
@@ -69,34 +96,46 @@ export const Navbar = () => {
             </>
           )}
         </HamburgerMenuButton>
-          <NavItem>
-              <NavLinkStyledTitle to="/" aria-label="Go to EcoTrip homepage">EcoTrip</NavLinkStyledTitle>
-            </NavItem>
-          <ThemeToggle />
-          {menuOpen && (
-            <OverlayMenu>
-              <OverlayMenuItem></OverlayMenuItem>
-              <OverlayMenuItem>
-                <NavLinkStyled to="/calculate" onClick={toggleMenu} aria-label="Go to the Calculate page">
-                  Calculate
-                </NavLinkStyled>
-              </OverlayMenuItem>
-              <OverlayMenuItem>
-                <NavLinkStyled to="/info" onClick={toggleMenu} aria-label="Learn more about eco-friendly travel">
-                  Information
-                </NavLinkStyled>
-              </OverlayMenuItem>
-              <OverlayMenuItem>
-                <NavLinkStyled to="/faq" onClick={toggleMenu} aria-label="Frequently asked questions">
-                  FAQ
-                </NavLinkStyled>
-              </OverlayMenuItem>
-              <OverlayMenuItem></OverlayMenuItem>
-            </OverlayMenu>
-          )}
-          
-        </Nav>
- 
+        <NavItem>
+          <NavLinkStyledTitle to="/" aria-label="Go to EcoTrip homepage" title="Go to Home page">
+            EcoTrip
+          </NavLinkStyledTitle>
+        </NavItem>
+        <ThemeToggle />
+        {menuOpen && (
+          <OverlayMenu>
+            <OverlayMenuItem></OverlayMenuItem>
+            <OverlayMenuItem>
+              <NavLinkStyled
+                to="/calculate"
+                onClick={toggleMenu}
+                aria-label="Go to the Calculate page"
+              >
+                Calculate
+              </NavLinkStyled>
+            </OverlayMenuItem>
+            <OverlayMenuItem>
+              <NavLinkStyled
+                to="/info"
+                onClick={toggleMenu}
+                aria-label="Learn more about eco-friendly travel"
+              >
+                Information
+              </NavLinkStyled>
+            </OverlayMenuItem>
+            <OverlayMenuItem>
+              <NavLinkStyled
+                to="/faq"
+                onClick={toggleMenu}
+                aria-label="Frequently asked questions"
+              >
+                FAQ
+              </NavLinkStyled>
+            </OverlayMenuItem>
+            <OverlayMenuItem></OverlayMenuItem>
+          </OverlayMenu>
+        )}
+      </Nav>
     </>
   );
 };
