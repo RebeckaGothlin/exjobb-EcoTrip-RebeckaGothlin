7cb3790c219bab3d264f0a2c35851956ddf424cd
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2025-01-20 14:08:22 +0100

    🐛 fix style bug

--- ecotrip/src/pages/NotFound.tsx
+++ ecotrip/src/pages/NotFound.tsx
@@ -6,26 +6,29 @@ import {
 } from "../components/styled/StyledNotFound";
 import { LinkButton } from "../components/styled/StyledButtons";
 import { Navbar } from "../components/Navbar";
+import { TextContainer } from "../components/styled/StyledContent";
 
 export const NotFound = () => {
   return (
     <>
       <Navbar></Navbar>
-      <NotFoundContainer>
-        <div className="img-container-404">
-          <img src="/404-error.png" alt="Error, page not found." />
-        </div>
-        <StyledTitle>Lost in the Wilderness? 🌍</StyledTitle>
-        <StyledDescription>
-          Looks like you’ve hit a wrong turn on the road of exploration. But
-          don’t worry, a quick reroute and you’ll be back on track—let’s travel
-          responsibly and find your way home!
-        </StyledDescription>
+      <TextContainer>
+        <NotFoundContainer>
+          <div className="img-container-404">
+            <img src="/404-error.png" alt="Error, page not found." />
+          </div>
+          <StyledTitle>Lost in the Wilderness? 🌍</StyledTitle>
+          <StyledDescription>
+            Looks like you’ve hit a wrong turn on the road of exploration. But
+            don’t worry, a quick reroute and you’ll be back on track—let’s
+            travel responsibly and find your way home!
+          </StyledDescription>
 
-        <Link to={"/"}>
-          <LinkButton>Back to Home</LinkButton>
-        </Link>
-      </NotFoundContainer>
+          <Link to={"/"}>
+            <LinkButton>Back to Home</LinkButton>
+          </Link>
+        </NotFoundContainer>
+      </TextContainer>
     </>
   );
 };
